eee4a4fe0c7b3cfe9c545787335bd35e38345080
suffergram <petrushinmd@ya.ru>
2022-06-14 16:10:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let upgrades = [
+const buildings = [
 	[15, 'GRAIN', 0.1],
 	[100, 'DUST', 1],
 	[1000, 'ROCK', 8],
@@ -13,33 +13,48 @@ let upgrades = [
 
 const ratio = 1.15
 let scoreIndex = 0
-let speed = 0
 let newInterval = 0
-let cheat = 0
+let cheat = 9999999999
 
 
-// create upgrade menu
+// create building menu
 let menu = document.createElement('div')
 menu.classList = 'menu'
 container.append(menu)
 menu.append(document.createElement('div'))
-menu.children[0].textContent = 'UPGRADES'
+menu.children[0].textContent = 'OBJECTS'
+menu.children[0].id = 'none'
 
-// create upgrades
-for (let x = 0; x < upgrades.length; x++) {
-	let upgrade = document.createElement('button')
-	upgrade.setAttribute('id', x)
-	upgrade.classList = 'upgrade'
-	upgrade.textContent = getNewDescription(x)
-	let upgradeCounter = document.createElement('div')
-	upgradeCounter.classList = 'upgradecounter'
-	upgradeCounter.textContent = 0
+// create buildings
+for (let x = 0; x < buildings.length; x++) {
+	buildings[x][3] = buildings[x][0]
 	menu.append(document.createElement('div'))
-	menu.children[x + 1].append(upgrade, upgradeCounter)
+
+	let building = document.createElement('div')
+	building.classList = 'building'
+	building.textContent = buildings[x][1]
+
+	let buildingCost = document.createElement('button')
+	buildingCost.id = x
+	buildingCost.classList = 'cost'
+	buildingCost.textContent = getNewCost(x)
+
+	let buildingCounter = document.createElement('div')
+	buildingCounter.classList = 'buildingcounter'
+	buildingCounter.textContent = 0
+	
+	menu.children[x + 1].append(buildingCost, buildingCounter, building)
 }
 
-let upgradesList = document.querySelectorAll('.upgrade')
-let upgradeCountersList = document.querySelectorAll('.upgradecounter')
+let buildingsList = document.querySelectorAll('.cost')
+let buildingCountersList = document.querySelectorAll('.buildingcounter')
+
+//create info section
+let info = document.createElement('div')
+info.classList = 'info'
+info.textContent = ''
+info.hidden = true
+container.append(info)
 
 // create player with .player class
 let player = document.createElement('div')
@@ -58,20 +73,25 @@ speedText.classList = 'speed'
 speedText.textContent = ''
 container.append(speedText)
 
-let score = +scoreText.textContent;
+let score = BigInt(+scoreText.textContent);
 
 
 // functions
 function newScore() {
-	scoreText.textContent = score ^ 0
+	scoreText.textContent = score
 }
 
 function newSpeed() {
-	speedText.innerHTML = 'current speed: ' + scoreIndex + ' points per ' + 1 + ' second'
+	speedText.innerHTML = '<strong>' + scoreIndex + '</strong> points per ' + 1 + ' second'
 }
 
-function getNewDescription(x) {
-	return upgrades[x][1] + ' (' + upgrades[x][0] + ') +' + upgrades[x][2]
+function getNewCost(x) {
+	let buildingPrice = buildings[x][3]
+	if (buildingPrice >= 10 ** 12) buildingPrice = (buildingPrice / 10 ** 12).toFixed(3) + 'T'
+	if (buildingPrice >= 10 ** 9) buildingPrice = (buildingPrice / 10 ** 9).toFixed(3) + 'B'
+	if (buildingPrice >= 10 ** 6) buildingPrice = (buildingPrice / 10 ** 6).toFixed(3) + 'M'
+
+	return '' + buildingPrice
 }
 
 function addScorePopOut() {
@@ -82,26 +102,26 @@ function addScorePopOut() {
 	setInterval(() => popOut.remove(), 1000)
 }
 
-function doUpgrade(x) {
-	if (score >= upgrades[x][0]) {
-		score -= upgrades[x][0]
+function addBuilding(x) {
+	if (score >= BigInt(buildings[x][3])) {
+		score -= BigInt(buildings[x][3])
 		newScore()
 
-		// change count of upgrade x
-		let count = +upgradeCountersList[x].textContent + 1
-		upgradeCountersList[x].textContent = count
+		// change count of building x
+		let count = +buildingCountersList[x].textContent + 1
+		buildingCountersList[x].textContent = count
 
 		// create new interval
-		scoreIndex += upgrades[x][2]
+		scoreIndex += buildings[x][2]
 		scoreIndex = +scoreIndex.toFixed(1)
 		newInterval = 10000 / scoreIndex / 10
 		clearInterval(autoClickInterval)
 		startInterval(scoreIndex)
 		newSpeed()
 
-		// change value of upgrade x
-		upgrades[x][0] = Math.floor(upgrades[x][0] * (ratio ** count))
-		upgradesList[x].textContent = getNewDescription(x)
+		// change value of building x
+		buildings[x][3] = Math.floor(buildings[x][0] * (ratio ** count))
+		buildingsList[x].textContent = getNewCost(x)
 	}
 }
 
@@ -119,7 +139,7 @@ function startInterval(x) {
 	}
 
 	autoClickInterval = setInterval(() => {
-		score += x
+		score += BigInt(Math.round(x))
 		newScore()
 	}, interval)
 }
@@ -129,12 +149,34 @@ function startInterval(x) {
 document.addEventListener('click', function(event) {
 	// if clicked on player
 	if (event.target.className == 'player') {
-		score += 1 + cheat
+		score += BigInt(1 + cheat)
 		newScore()
 		addScorePopOut()
 	}
 
-	else for (let i = 0; i < upgrades.length; i++) {
-		if (event.target.id == i) doUpgrade(i)
+	// if clicked on buldings
+	else for (let i = 0; i < buildings.length; i++) {
+		if (event.target.id == i) addBuilding(i)
 	}
 })
+
+// mouseover handler
+menu.addEventListener('mouseover', function(event) {
+	if (event.target.className == 'cost') {
+		// event.target.id
+		info.innerHTML = '<p>' + event.target.parentNode.querySelector('.building').innerHTML + '</p>\n'
+		info.innerHTML += '<p>makes ' + buildings[+event.target.id][2] + ' points per second</p>\n'
+		info.innerHTML += '<p>amount: ' + event.target.parentNode.querySelector('.buildingcounter').innerHTML + '</p>\n'
+		info.innerHTML += '<p>current cost: ' + buildings[+event.target.id][3] + '</p>\n'
+
+		info.hidden = false
+	}
+})
+
+// menu.addEventListener('mouseout', function(event) {
+// 	if (event.target.className == 'cost') {
+// 		info.hidden = true
+// 	}
+// })
+
+
