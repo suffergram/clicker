b8e40280d4b177fb63181df1daf5e36e1923d876
suffergram <petrushinmd@ya.ru>
2022-06-26 21:58:18 +0300

    Update so.js

--- so.js
+++ so.js
@@ -1,1 +1,9 @@
-alert('hey there!')
+let shimmers = document.querySelector('#shimmers')
+
+function check() {
+	if (shimmers.children.length == 0) console.log('there are no shimmers')
+}
+
+check()
+
+
