55650ce0b345203ffdaabf943b6490d5c186def4
suffergram <petrushinmd@ya.ru>
2022-06-26 23:37:15 +0300

    Update so.js

--- so.js
+++ so.js
@@ -2,8 +2,9 @@ let shimmers = document.querySelector('#shimmers')
 
 function check() {
 	if (shimmers.children.length == 0) console.log('there are no shimmers')
+	else console.log('there ARE shimmer!!!')
 }
 
-check()
+setTimer(check, 100)
 
 
