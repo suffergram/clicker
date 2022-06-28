0f3b29be57c8bc4b2b1a036296520d470e5c5cfe
suffergram <petrushinmd@ya.ru>
2022-06-28 22:07:48 +0300

    Update so.js

--- so.js
+++ so.js
@@ -2,13 +2,13 @@ let shimmers = document.querySelector('#shimmers')
 let cookie = document.querySelector('#bigCookie')
 
 function check() {
-	if (shimmers.children.length == 0) return
-	else {
+	if (shimmers.children.length != 0) {
 		let shimmer = document.querySelectorAll('.shimmer')
 		for (let item of shimmer) {
 			item.click()
 		}
 	}
+	
 	let fortune = document.querySelector('.fortune')
 	if (!!fortune) fortune.click()
 }
